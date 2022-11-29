47794faa918719d485585cf529305d30ac6e039a
Yana Chernushevich <yanachernushevich@gmail.com>
2022-11-29 13:31:17 +0000

    new dz

---
+++ 12les/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class Animal {
+	constructor(name) {
+		this.name = name;
+	}
+}
+
+class Rabbit extends Animal {
+	constructor(name) {
+		super(name);
+		this.created = Date.now();
+	}
+}
+
+let rabbit = new Rabbit('White rabbit');
+
+alert(rabbit.name);
